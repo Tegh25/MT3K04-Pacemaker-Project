a83e75cee60b5f82f7251656052f465613f0ee6d
fuzail <fuzailm@mcmaster.ca>
2023-10-12 02:55:25 -0400

    adding base files so now browser window pops up with basic messages

---
+++ front-end-assets/ElectronApp/my_electron_react_application/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to your PaceMaker</h1>
+      <button >Sign in</button>
+    </div>
+  );
+}
+
+
+export default App;
